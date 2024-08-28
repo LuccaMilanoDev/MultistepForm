c50a825094e8d86230faebc23ae0ff9001bb5321
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-08-28 19:14:01 -0300

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+// hooks
+import { useForm } from "./hooks/useForm";
+
 import "./App.css";
 
 function App() {
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
+  const { currentStep, currentComponent } = useForm(formComponents);
   return (
-    <>
-      <h1>Project MultiStepForm</h1>
-    </>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com sua compra, utilize o formulario abaixo para
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form>
+          <div className="inputs-container">{currentComponent}</div>
+          <div className="actions">
+            <button type="button">
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
 
