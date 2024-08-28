c6c91e1b5536d2e1125fd202513fd49f4f108d8b
LuccaMilanoDev <luccamilano2018@gmail.com>
2024-08-28 19:48:53 -0300

    Avançar and Voltar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
+import { FiSend } from "react-icons/fi";
 
 // hooks
 import { useForm } from "./hooks/useForm";
@@ -10,7 +11,8 @@ import "./App.css";
 
 function App() {
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
-  const { currentStep, currentComponent } = useForm(formComponents);
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(formComponents);
   return (
     <div className="app">
       <div className="header">
@@ -22,17 +24,26 @@ function App() {
       </div>
       <div className="form-container">
         <p>Etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type="button">
-              <GrFormPrevious />
-              <span>Voltar</span>
-            </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
